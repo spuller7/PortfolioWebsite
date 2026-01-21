95c6aaf1aec712e07d50e97d08cc7b0a98d72b4e
Travis Spuller <travis@ticketsocket.com>
2026-01-21 00:57:49 -0500

    update

---
+++ precache-manifest.dbc390bf639726bd4b89dca0bcae676c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0df2061a92545d2276169521c8a7060",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1355f1a96a278a9dc11",
+    "url": "/static/css/2.103191dd.chunk.css"
+  },
+  {
+    "revision": "81dbec9479e015c79a8f",
+    "url": "/static/css/main.f7e31461.chunk.css"
+  },
+  {
+    "revision": "a1355f1a96a278a9dc11",
+    "url": "/static/js/2.79447b99.chunk.js"
+  },
+  {
+    "revision": "58dc12295c1e0ca74bc155bdfb8f6b90",
+    "url": "/static/js/2.79447b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81dbec9479e015c79a8f",
+    "url": "/static/js/main.c214986a.chunk.js"
+  },
+  {
+    "revision": "3f2a823c78a091034a05",
+    "url": "/static/js/runtime-main.b6c5c5b2.js"
+  }
+]);
